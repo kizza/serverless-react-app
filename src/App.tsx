b8480ffd8223e4d2d38991dffa7bdccebfd24eee
Keiran <keiran.oleary@stacktracehq.com>
2020-03-18 22:04:38 +1000

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, NavLink, Route, Switch} from "react-router-dom";
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </Route>
+            <Route path="/about">
+              <p>About page</p>
+            </Route>
+            <Route path="/faq">
+              <p>FAQ page</p>
+            </Route>
+          </Switch>
+
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/faq">FAQ</NavLink>
+            </li>
+          </ul>
+
+        </Router>
       </header>
     </div>
   );
